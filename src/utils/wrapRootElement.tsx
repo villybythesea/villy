f915009c154520b5789ecc989616b38e021513e1
Konstantin Dichev <konstantin.dichev@gmail.com>
2020-02-18 17:56:48 +0100

    add authenticator

--- src/utils/wrapRootElement.tsx
+++ src/utils/wrapRootElement.tsx
@@ -3,11 +3,13 @@ import { client } from "./../client"
 import { ApolloProvider } from "@apollo/react-hooks"
 import { CssBaseline, Box } from "@material-ui/core"
 import { Navigation } from "./../components/Navigation"
+import { Authenticator } from "../pages/reservations"
 
 export const wrapRootElement = ({ element }: any) => (
   <ApolloProvider client={client}>
     <CssBaseline />
     <Navigation />
     <Box style={{ margin: 16 }}>{element}</Box>
+    <Authenticator />
   </ApolloProvider>
 )
