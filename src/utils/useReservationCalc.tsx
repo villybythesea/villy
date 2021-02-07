7b9977d6c27b99ea1ce4f16eac8aca185a410f68
Konstantin Dichev <konstantin.dichev@gmail.com>
2021-02-07 15:15:54 +0100

    add 2021 pricing model (#375)
    * add 2021 pricing model
    
    * fix check month moment object
    
    * add october

--- src/utils/useReservationCalc.tsx
+++ src/utils/useReservationCalc.tsx
@@ -2,27 +2,58 @@ import { extendMoment } from "moment-range"
 const Moment = require("moment")
 const moment = extendMoment(Moment)
 
-const pricing = {
-  double: {
-    MAY: 40,
-    JUNE: 46,
-    JULY: 60,
-    AUGUST: 60,
-    SEPTEMBER: 50,
+const yearlyPricing = {
+  2020: {
+    double: {
+      MAY: 40,
+      JUNE: 46,
+      JULY: 60,
+      AUGUST: 60,
+      SEPTEMBER: 50,
+      OCTOBER: 50,
+    },
+    tripple: {
+      MAY: 60,
+      JUNE: 69,
+      JULY: 90,
+      AUGUST: 90,
+      SEPTEMBER: 75,
+      OCTOBER: 75,
+    },
+    apartment: {
+      MAY: 90,
+      JUNE: 92,
+      JULY: 120,
+      AUGUST: 120,
+      SEPTEMBER: 100,
+      OCTOBER: 100,
+    },
   },
-  tripple: {
-    MAY: 60,
-    JUNE: 69,
-    JULY: 90,
-    AUGUST: 90,
-    SEPTEMBER: 75,
-  },
-  apartment: {
-    MAY: 90,
-    JUNE: 92,
-    JULY: 120,
-    AUGUST: 120,
-    SEPTEMBER: 100,
+  2021: {
+    double: {
+      MAY: 48,
+      JUNE: 60,
+      JULY: 65,
+      AUGUST: 65,
+      SEPTEMBER: 60,
+      OCTOBER: 60,
+    },
+    tripple: {
+      MAY: 70,
+      JUNE: 90,
+      JULY: 98,
+      AUGUST: 98,
+      SEPTEMBER: 90,
+      OCTOBER: 90,
+    },
+    apartment: {
+      MAY: 95,
+      JUNE: 120,
+      JULY: 130,
+      AUGUST: 130,
+      SEPTEMBER: 120,
+      OCTOBER: 120,
+    },
   },
 }
 
@@ -40,39 +71,47 @@ export const useReservationCalc = ({ rooms, arrival_date, departure_date }) => {
     return (
       total +
       arrayOfDates.reduce((acc, day) => {
+        const yearOfReservation = moment(day).format("YYYY")
         const containsMay =
-          moment()
+          moment(yearOfReservation)
             .month(4)
             .range("month")
             .contains(moment(day)) && "MAY"
         const containsJune =
-          moment()
+          moment(yearOfReservation)
             .month(5)
             .range("month")
             .contains(moment(day)) && "JUNE"
         const containsJuly =
-          moment()
+          moment(yearOfReservation)
             .month(6)
             .range("month")
             .contains(moment(day)) && "JULY"
         const containsAugust =
-          moment()
+          moment(yearOfReservation)
             .month(7)
             .range("month")
             .contains(moment(day)) && "AUGUST"
         const containsSeptember =
-          moment()
+          moment(yearOfReservation)
             .month(8)
             .range("month")
             .contains(moment(day)) && "SEPTEMBER"
+        const containsOctober =
+          moment(yearOfReservation)
+            .month(9)
+            .range("month")
+            .contains(moment(day)) && "OCTOBER"
+
         return (
           acc +
-          pricing[room.room_type.room_type][
+          yearlyPricing[yearOfReservation][room.room_type.room_type][
             containsMay ||
               containsJune ||
               containsJuly ||
               containsAugust ||
-              containsSeptember
+              containsSeptember ||
+              containsOctober
           ]
         )
       }, 0)
