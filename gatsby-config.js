4cfa77b321b59e27fe402938afecc83e10feff3e
kdichev <konstantin.dichev@gmail.com>
2021-08-13 18:40:17 +0200

    add manifest changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        name: `Вили Ла Коста График`,
+        short_name: `Вили Ла Коста График`,
+        start_url: `/schedule`,
+        background_color: `#3f51b5`,
+        theme_color: `#3f51b5`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/vililogo.png`, // This path is relative to the root of the site.
       },
     },
     "gatsby-plugin-typescript",
